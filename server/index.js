5ea67af9bce6a726c3a21fdc6326bdd1ba23bedf
Trevor Buchanan <trevor@trevorbuchanan.com>
2021-03-25 16:52:21 -0500

    Added Cleanup of old entries and squashed a ton of bugs

--- server/index.js
+++ server/index.js
@@ -40,6 +40,19 @@ function addLocation(ip, ipObj) {
     })
 }
 
+
+
+function removeOld(ipObj) {
+  let yesterday = new Date();
+  yesterday.setDate(yesterday.getDate() - 1);
+  const filtered = Object.keys(ipObj).filter(ip => {
+    return ipObj[ip].latestAttempt / 1000 > yesterday;
+  })
+  ipObj = filtered.reduce((obj, ip) => {
+    return { ...obj, [ip]: ipObj[ip] };
+  }, {});
+}
+
 // SETUP ENDPOINT
 app.listen(PORT, () => {
   log(`listening on port ${PORT}`);
@@ -52,27 +65,35 @@ app.get('/', (req, res) => {
 });
 
 // MONITOR LOG AND CREATE `ipAndData` OBJECT
-const journalctl = new Journalctl({unit: "ssh", since: "1 hour ago"});
+const journalctl = new Journalctl({unit: "ssh", since: "yesterday"});
 let ipAndData = {};
 journalctl.on('event', (event) => {
   // only work with events that contain 'disconnect' in the message
   // as well as 'authenticating' or 'invalid'
-  if (event.MESSAGE.match(/disconnect.*[authenticating,invalid]/gi)) {
+  if (event.MESSAGE.match(/disconnect.*(authenticating|invalid).*/gi)) {
     const ipRegex = /(?:[0-9]{1,3}\.){3}[0-9]{1,3}/g;
     let ip = event.MESSAGE.match(ipRegex);
     if (ip) {
-      // possible the 'name' is a valid ip address
+      // possible the 'name' field is a valid ip address
       // so this ensures we get the actual ip
       ip = ip[ip.length -1];
-      ipAndData[ip] = {
-        latestAttempt: event.SYSLOG_TIMESTAMP,
+      if (ipAndData[ip] == undefined) {
+        ipAndData[ip] = {};
       }
-      if (!ipAndData[ip].location) {
-         addLocation(ip, ipAndData[ip]);
+      if (ipAndData[ip].location == undefined) {
+        addLocation(ip, ipAndData[ip]);
       }
-      if (Date.now() - (event.__REALTIME_TIMESTAMP / 1000) < 60000 ) {
+      if (ipAndData[ip].count == undefined) {
+        ipAndData[ip].count = 0;
+      }
+      ipAndData[ip].latesetAttempt = event.__REALTIME_TIMESTAMP,
+      ipAndData[ip].count += 1;
+      if (Date.now() - (ipAndData[ip].latestAttempt / 1000) < 60000 ) {
         log(`new IP: ${ip}`);
       }
+      removeOld(ipAndData);
     }
   }
 })
+
+
